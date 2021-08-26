bbe1ce58b53ac7c5025a04e15d7ec5b8ee7443c5
Sam Parsons <samparsons269@gmail.com>
2021-08-26 19:31:50 -0400

    formatted code with prettier

--- __tests__/explicit-comparator.js
+++ __tests__/explicit-comparator.js
@@ -1,19 +1,23 @@
-const rule = require('../lib/rules/explicit-comparator.js');
-const RuleTester = require('eslint').RuleTester;
+const rule = require("../lib/rules/explicit-comparator.js");
+const RuleTester = require("eslint").RuleTester;
 
 const ruleTester = new RuleTester();
-ruleTester.run('explicit-comparator', rule, {
-  valid: [
-    `[1, 10, 100]`,
-    `["1", "10", "100"]`,
-    `[true, true, false]`,
-  ].map(array => ({code: `var sorted = ${array}.sort(function(a,b) { return a - b })`,})),
+ruleTester.run("explicit-comparator", rule, {
+  valid: [`[1, 10, 100]`, `["1", "10", "100"]`, `[true, true, false]`].map(
+    (array) => ({
+      code: `var sorted = ${array}.sort(function(a,b) { return a - b })`,
+    })
+  ),
 
-  invalid: [
-    `[1, 10, 100]`,
-    `["1", "10", "100"]`,
-    `[true, true, false]`,
-  ].map(array => ({code: `var sorted = ${array}.sort()`,
-    errors: [{message: "Use a comparator argument when calling Array.prototype.sort"}]}
-  ))
+  invalid: [`[1, 10, 100]`, `["1", "10", "100"]`, `[true, true, false]`].map(
+    (array) => ({
+      code: `var sorted = ${array}.sort()`,
+      errors: [
+        {
+          message:
+            "Use a comparator argument when calling Array.prototype.sort",
+        },
+      ],
+    })
+  ),
 });
